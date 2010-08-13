e714f46ab360a213557c5ab2946f67d184f2ed06
Ben Coe <bencoe@bencoe.2ndsiteinc.com>
2010-08-13 10:19:50 -0400

    Removed StringIO which wasn't being used.

--- smartytotwig/__init__.py
+++ smartytotwig/__init__.py
@@ -21,7 +21,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
-import fileinput, StringIO
+import fileinput
 from smartytotwig.smarty_grammar import smarty_language
 from smartytotwig.pyPEG import parse, parseLine, parser
 
