ce23038f039050db66b06b8af261a1a4f81b5bc1
Ben Coe <bencoe@bencoe.2ndsiteinc.com>
2010-08-13 10:06:05 -0400

    Fixing up command line tool.

--- smartytotwig/__main__.py
+++ smartytotwig/__main__.py
@@ -24,34 +24,38 @@ THE SOFTWARE.
 import optparse, sys, smartytotwig
 from smartytotwig.tree_walker import TreeWalker
 
-opt1 = optparse.make_option(
-    "-s",
-    "--smarty-file",
-    action="store",
-    dest="source",
-    help="Path to the source Smarty file."
-)
+def main():    
+    opt1 = optparse.make_option(
+        "-s",
+        "--smarty-file",
+        action="store",
+        dest="source",
+        help="Path to the source Smarty file."
+    )
 
-opt2 = optparse.make_option(
-    "-t",
-    "--twig-file",
-    action="store",
-    dest="target",
-    help="Location of the Twig output file."
-)    
-                
-if __name__ == "__main__":
+    opt2 = optparse.make_option(
+        "-t",
+        "--twig-file",
+        action="store",
+        dest="target",
+        help="Location of the Twig output file."
+    )
+    
     parser = optparse.OptionParser(usage='smartytotwig --smarty-file=<SOURCE TEMPLATE> --twig-file=<OUTPUT TEMPLATE>')
     parser.add_option(opt1)
     parser.add_option(opt2)
     (options, args) = parser.parse_args(sys.argv)
-                
+            
     if options.source and options.target:
         ast = smartytotwig.parse_file(options.source)
         tree_walker = TreeWalker(ast)
-                
+            
         f = open(options.target, 'w+')
         f.write(tree_walker.code)
         f.close()
-        
+    
         print 'Template outputted to %s' % options.target
+    
+                
+if __name__ == "__main__":
+    main()
